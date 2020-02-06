8aef411e7e891ba69850912d7ac7fdea23910050
Student <student@eda-bc-02.localdomain>
2020-02-07 11:57:02 +1300

    weapon placeholder

--- client/components/Goats.jsx
+++ client/components/Goats.jsx
@@ -3,34 +3,37 @@ import {Link} from 'react-router-dom'
 import { getGoat } from '../api/api'
 import data from '../../data'
 
+
 class Goats extends React.Component{
   constructor(props){
     super(props)
 
     this.state = {
-        
+
     }
   }
-  
 
   componentDidMount() {
-    // // fetch("http://placegoat.com/200/200")
-    // getGoat() 
-    // .then(image => {   
-    //     const goats = image.data
-    //     console.log(image.data)
-    //     this.setState({
-    //         goatArr: goats 
-    //     }) 
-    // })
+
   }
 
-
   render() {
+
+    const weapon = data[Math.ceil(Math.random()* data.length)]
+
     return (
+
         <React.Fragment>
-            <img className='goatImg' src="http://placegoat.com/400/400" />
-            
+            <img className='images' src="http://placegoat.com/400/400" /> 
+
+            <div className='btn'></div>
+            <img className='images'src={weapon.image} alt="img"/>
+            <div className='weapon'>
+                <ul>
+                    <p>{weapon.name}</p>
+                    <p>{weapon.description}</p>
+                </ul>
+            </div>
         </React.Fragment>
     )
   }
