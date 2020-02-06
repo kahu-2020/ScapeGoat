52bd9ebdd17edb0696772705f4835c1bc60e66e2
Student <student@eda-bc-02.localdomain>
2020-02-07 10:26:26 +1300

    initial branch commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
+
+import Goats from './Goats'
+
 class App extends React.Component{
   
   render() {
@@ -8,6 +11,7 @@ class App extends React.Component{
       <Router>
         <React.Fragment>
           {/* Components go here */}
+          <Route path='/goat' component={Goats}/>
         </React.Fragment>
       </Router>
     )
--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const path = require('path')
 const express = require('express')
+const request = require('superagent')
 
 const server = express()
 
+const goatUrl = 'http://placegoat.com/'
+
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.get('/:width/:height', (req,res) => {
+    request
+    .get(goatUrl + req.params.width + '/' + req.params.height)
+    .then(res => res.body)
+    .then(apiRes => res.json(apiRes))
+})
 
 module.exports = server
