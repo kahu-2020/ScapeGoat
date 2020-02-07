824418a66a748a7f3e4a5cc902aa506c2853375d
Student <student@eda-bc-01.localdomain>
2020-02-07 14:29:58 +1300

    ternary button

--- client/components/Goats.jsx
+++ client/components/Goats.jsx
@@ -19,9 +19,19 @@ class Goats extends React.Component {
 
     render() {
 
-        const weapon = data[Math.ceil(Math.random() * data.length)]
+        const weapon = data[Math.ceil(Math.random() * data.length -1)]
+        let endLink
+
+        if(weapon.name == 'Vacuum Cleaner') {
+            endLink = <Link to='/win'><button className='btn btn-warning' >Fight!</button></Link>
+
+        } else {
+          endLink = <Link to='/lose'><button className='btn btn-warning' >Fight!</button></Link>
+        }
+        
 
         return (
+  
             <div>
                 <div className='container'>
                     <div className='info'>
@@ -31,7 +41,7 @@ class Goats extends React.Component {
                     <img className='images' src={weapon.image} alt="img" />
                     <img className='images' src="http://placegoat.com/400/400" />
                 </div>
-                <Link to={'/win'}><button className='btn btn-warning' >Fight!</button></Link>
+                {endLink}
             </div>
         )
     }
